c84b5b22247933188218ce23ce3ff37aac24a360
ANANDNJIT <aplv180@gmail.com>
2023-08-08 18:11:58 -0400

    update the java file

--- Assets/script.js
+++ Assets/script.js
@@ -10,17 +10,57 @@ let temp = document.querySelector('.temp');
 
 searchForm.addEventListener("submit", function(event){
   event.preventDefault();
-    
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity.value}&appid=${f2121762395955c7d6494cc0e3d06558}&units=metric`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity.value}&appid=${apiKey}&units=imperial`)
     .then(response => response.json())
     .then(displayData)
     .catch(err => {
-      console.error(err);
       alert("Error fetching weather data. Please check the city name.");
     });
 });
 
+
+
+
 var displayData = (weather) => {
-  temp.innerText = `${weather.main.temp}°C`;
+  console.log(weather);
+
+  /* Display current weather data here */
+  todayWeather.innerHTML = `
+    <h2>Current Weather</h2>
+    
+    <p>Temperature: ${weather.main.temp}°F</p>
+    <p>Humidity: ${weather.main.humidity}%</p>
+    <p>Wind Speed: ${weather.wind.speed} m/s</p>
+    <p>Date: ${formattedDate}</p>
+  `;
+
+  /* display the  5-day forecast data  here*/ 
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchCity.value}&cnt=5&appid=${apiKey}&units=imperial`)
+    
+  
+  .then(response => response.json())
+    .then(displayForecast)
+    .catch(err => {
+      console.error("Error fetching forecast data.", err);
+    });
+};
+
+var displayForecast = (forecast) => {
+  console.log(forecast);
+
+  
+  forecastWeather.innerHTML = `
+    <h2>5-Day Forecast</h2>
+    <ul>
+      ${forecast.list.map(item => `
+        <li>
+          Date: ${item.dt}
+          <p>Temperature: ${item.main.temp}°F</p>
+          <p>Humidity: ${item.main.humidity}%</p>
+          <p>Wind Speed: ${item.wind.speed} m/s</p>
+        </li>
+      `).join('')}
+    </ul>
+  `;
 };
