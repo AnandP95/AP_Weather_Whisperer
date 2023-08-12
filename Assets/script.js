8bcd006809238eb977a4fe3b1e1cd5520249626e
ANANDNJIT <aplv180@gmail.com>
2023-08-11 22:45:43 -0400

    update the html and javascript file

--- Assets/script.js
+++ Assets/script.js
@@ -1,87 +1,163 @@
+
+/* API key for fetch data from open weatherMap */
 var apiKey = "f2121762395955c7d6494cc0e3d06558";
-var  searchForm = document.getElementById("search_box");
-var  searchCity = document.getElementById("city-input");
-var  history = document.getElementById("history_list");
-var todayWeather =  document.getElementById("current-weather");
+
+/* declare the variable here */
+var searchForm = document.getElementById("search_box");
+var searchCity = document.getElementById("city-input");
+var searchButton = document.getElementById("searchButton");
+var historyList = document.getElementById("history_list");
+
+var searchedCityEl = document.getElementById("searchedCity");
+var todayWeather = document.getElementById("current-weather");
 var forecastWeather = document.getElementById("forecast");
-let temp = document.querySelector('.temp');
+var historyCities = document.querySelectorAll(".history-city");
 var searchHistory = [];
 
 
-searchForm.addEventListener("submit", function(event){
+/* event listener for form submit */
+searchForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
+  /* get the city name from this field*/
   var cityName = searchCity.value;
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity.value}&appid=${apiKey}&units=imperial`)
-    .then(response => response.json())
-    .then(data => {
-    
-    displayData(data);
+  fetch(
 
-    addTohistory(cityName);})
+    /* fetch the weather data using this websites */
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      /* this variable used for display the current weather data */
+      displayData(data);
 
-    .catch(err => {
+        /* this variable used for add city to search history section */
+      addToHistory(cityName);
+
+      /* Fetch and display weather data for the selected city*/ 
+      fetchWeatherData(cityName);
+    })
+    .catch((err) => {
       alert("Error fetching weather data. Please check the city name.");
     });
 });
 
 
-function  addTohistory(cityName) {
-
-  if(!searchHistory.includes(cityName)){
+ /* Set a function for add city input to  search history */
+function addToHistory(cityName) {
+  
+  if (!searchHistory.includes(cityName)) {
     searchHistory.push(cityName);
-    updatehistory();
+     updateHistory();
+      saveHistory();
+  }
+}
+
+/* Set a function for save input search history to local storage */
+function saveHistory(){
+  localStorage.setItem("searchHistory",JSON.stringify(searchHistory));
+}
+
+/* seta function for update a search history */
+function updateHistory() {
+  historyList.innerHTML = searchHistory
+    .map((city) => `<li class="history-city">${city}</li>`)
+    .join("");
+
+
+    /* Update the list of history cities */ 
+  historyCities = document.querySelectorAll(".history-city"); 
+  
+  historyCities.forEach((historyCity) => {
+    historyCity.addEventListener("click", () => {
+      var cityName = historyCity.textContent; 
+
+      fetchWeatherData(cityName); 
+    });
+  });
+}
+
+
+
+/* set a Function to load search history from local storage*/ 
+function loadHistory() {
+  const storedHistory = localStorage.getItem("searchHistory");
+
+  if (storedHistory) {
+    
+    searchHistory = JSON.parse(storedHistory);
+    updateHistory();
   }
 }
 
 
-function addTohistory(){
-history.innerHTML = searchHistory.map(city=> `<li>${city}</li>`).join('');
 
+function fetchWeatherData(cityName) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`)
+    .then(response => response.json())
+    .then(data => {
+      displayData(data);
+    })
+    .catch(err => {
+      alert("Error fetching weather data. Please check the city name.");
+    });
 }
-var displayData = (weather) => {
-  console.log(weather);
 
+document.addEventListener("DOMContentLoaded", loadHistory);
+
+
+var displayData = (weather) => {
+  var currentDate = new Date();
+  var formattedDate = currentDate.toLocaleDateString();
   /* Display current weather data here */
   todayWeather.innerHTML = `
     <h2>Current Weather</h2>
-    
+    <ul>
+    <li>
+    <p>${weather.name}</p>
+    <p>${formattedDate}</p>
+    <img src="https://openweathermap.org/img/w/${weather.weather[0].icon}.png" alt="Weather Icon">
     <p>Temperature: ${weather.main.temp}°F</p>
     <p>Humidity: ${weather.main.humidity}%</p>
     <p>Wind Speed: ${weather.wind.speed} m/s</p>
+    </li>
+    </ul>
   `;
 
-  /* display the  5-day forecast data  here*/ 
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchCity.value}&cnt=1&appid=${apiKey}&units=imperial`)
-    .then(response => response.json())
+  /* Display the 5-day forecast data here */
+  
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${weather.name}&appid=${apiKey}&units=imperial`
+  )
+    .then((response) => response.json())
     .then(displayForecast)
-    .catch(err => {
+    .catch((err) => {
       console.error("Error fetching forecast data.", err);
     });
 };
 
+var displayForecast = (forecast) => {
+  console.log(forecast);
 
+  const filteredForecast = forecast.list.filter((item, index) => index % 8 === 0);
 
-  /*  Display 5-day forecast data */
-  var displayForecast = (forecast) => {
-    console.log(forecast);
-    // Display 5-day forecast data
-    forecastWeather.innerHTML = `
+  forecastWeather.innerHTML = `
       <h2>5-Day Forecast</h2>
       <ul>
-        ${forecast.list.map(item => {
-          var urlIcon = `https://openweathermap.org/img/w/${item.weather[0].icon}.png`;
-  
-          return `
-          <li>
-            Date: ${item.dt_txt}
-            <p>Temperature: ${item.main.temp}°F</p>
-            <p>Humidity: ${item.main.humidity}%</p>
-            <p>Wind Speed: ${item.wind.speed} m/s</p>
-            <img src=${urlIcon}>
-          </li>
-        `})
-        }
+          ${filteredForecast.map(item => {
+              var urlIcon = `https://openweathermap.org/img/w/${item.weather[0].icon}.png`;
+              var forecastDate = new Date(item.dt_txt);
+              var formattedDate = forecastDate.toLocaleDateString('en-US', {  month: 'numeric',  day: 'numeric',year: 'numeric' });
+
+              return `
+              <li>
+                  <p>Date: ${formattedDate}</p>
+                  <img src=${urlIcon}>
+                  <p>Temperature: ${item.main.temp}°F</p>
+                  <p>Humidity: ${item.main.humidity}%</p>
+                  <p>Wind Speed: ${item.wind.speed} m/s</p>
+              </li>`;
+          }).join('')}
       </ul>
-    `;
-  };
+  `;
+};
--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # AP_Weather_Whisperer
+
+
+
+
+
+
+## Description 
+This website is a  weather dashboard . That allows users to search for weather information for different cities.  view the current weather as well as a 5-day forecast and saved a search history.  
+
+## Table of Contents
+
+
+- [AP\_Weather\_Whisperer](#ap_weather_whisperer)
+  - [Description](#description)
+  - [Table of Contents](#table-of-contents)
+  - [Technology used](#technology-used)
+  - [Features](#features)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [view concept](#view-concept)
+  - [License](#license)
+
+
+
+## Technology used 
+
+* Html 
+* CSS
+* JavaScript
+* Openweather API for whether data 
+  
+  
+## Features 
+* Users can Search for weather information by entering a city name.
+* Its Display the current weather conditions including temperature, humidity, and wind speed by city search.
+* Display a 5-day forecast showing temperature, humidity, and wind speed trends.
+* Access a search history list that records your previous city searches.
+* Click on a city in the search history to automatically  retrieve its weather information.
+
+
+## Installation
+
+For doing this challenges , first i install the VS Code developer tool to edit and modify the starter code. 
+
+## Usage
+
+1. clone the AP_Weather_Whisperer repository to your local machine.
+2. Open the "index.html" file in your browser. 
+3. Enter the name of a city in the search input and click the "Search" button to get weather information
+4. The current weather details and a 5-day forecast will be displayed.
+5. here is demo of app : ![Weather Application  ](Assets/05-third-party-apis-homework-demo.gif)
+
+
+
+## Credits
+* i clone a starter code from this repository: [https://github.com/coding-boot-camp/crispy-octo-meme]
+
+* i watched this video for push the code from vs code to git hub and deployed at live .[https://www.youtube.com/watch?v=Ky_aUrzArt4]
+
+* In this website , its very helpful to understand how to make a javascript click event listener .[https://developer.mozilla.org/en-US/docs/Web/API/Element/click_event]
+
+*  In this website, i learned a then method.[https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then] 
+
+* in this website, i learned a how to fetch the data.[https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch] 
+ * The weather data is fetched from the OpenWeatherMap API. [https://openweathermap.org/forecast5]
+ * i learned from here, how to formatted the date in numeric format [https://stackoverflow.com/questions/27939773/tolocaledatestring-short-format]
+
+ ## view concept 
+ 
+
+ 
+## License
+N/A
